a8f6a41849b103180412e704508eaf351c5afec7
Jonas Saabel <jonassaabel@gmail.com>
2022-02-05 01:27:47 +0100

    ...

---
+++ book/chapter2/FormatTest.java
@@ -0,0 +1,11 @@
+public class FormatTest{
+    public static void main(String[] args){
+     
+        double a = 0.123456789;
+        int b = 7;
+        String c = "Hello hello hello";
+        System.out.printf("%.2f", a); // OBS: "printf"!
+
+    }
+
+}    
