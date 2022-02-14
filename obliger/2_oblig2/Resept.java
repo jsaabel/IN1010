9b089c74d0ce139b5c58723a9056da6580b6f783
jsaabel <jonassaabel@gmail.com@git config --global user.email jonassaabel@gmail.comgit config --global user.email jonassaabel@gmail.com@>
2022-02-15 00:11:43 +0100

    Del A og B ferdig (untatt tegninger)

---
+++ obliger/2_oblig2/Resept.java
@@ -0,0 +1,164 @@
+/**
+ * Abstract klasse som brukes som grunnlag for ulike typer resepter.
+ */
+public abstract class Resept{
+
+    protected int id;
+    protected Legemiddel legemiddel;
+    protected Lege utskrivendeLege;
+    protected int pasientId;
+    protected int reit;
+
+    protected static int sisteId;
+
+    protected String farge;
+
+    /**
+     * KONSTRUKTOER
+     */
+    public Resept(Legemiddel legemiddel, Lege utskrivendeLege, int pasientId,
+            int reit){
+
+        sisteId++;
+
+        this.legemiddel = legemiddel;
+        this.utskrivendeLege = utskrivendeLege;
+        this.pasientId = pasientId;
+        this.reit = reit;
+
+        this.id = sisteId;
+    }
+
+    /**
+     * Metoder som henter relevant data (Oppgave B2)
+     */
+    public int hentId(){
+        return this.id;
+    }
+
+    public Legemiddel hentLegemiddel(){
+        return this.legemiddel;
+    }
+
+    public Lege hentLege(){
+        return this.utskrivendeLege;
+    }
+
+    public int hentReit(){
+        return this.reit;
+    }
+
+    /**
+     * Flere metoder (Oppgave B2)
+     */
+    public boolean bruk(){
+    
+        if (this.hentReit() > 0){
+            this.reit --;
+            return true;}
+        return false;
+    }
+
+    public abstract String farge();
+    public abstract int prisAaBetale();
+
+    /**
+     * Overskriving av toString-metoden
+     */
+    public String toString(){
+
+        String standardInfo = "Resept " + this.hentId() + " " + 
+            "(" + this.farge() + ")" + ": "
+            + this.legemiddel.hentNavn() + ". "
+            + "Reit: " + this.hentReit() + ". "
+            + "Pris aa betale: " + this.prisAaBetale() + ".";
+
+        if (this.hentReit() < 1){
+            return "Ugyldig resept!";}
+
+        return standardInfo;
+    }
+}
+
+/**
+ * Klasse for hvite resepter.
+ */
+class HvitResept extends Resept{
+
+    public String farge(){
+        return "hvit";
+    }
+
+    public int prisAaBetale(){
+        return 0;
+    }
+
+    public HvitResept(Legemiddel legemiddel, Lege utskrivendeLege,
+            int pasientId, int reit){
+
+        super(legemiddel, utskrivendeLege, pasientId, reit);
+    }
+}
+
+/**
+ * Klasse for Militaerresepter.
+ */
+class MilResept extends HvitResept{
+
+    public MilResept(Legemiddel legemiddel, Lege utskrivendeLege,
+            int pasientId){
+        super(legemiddel, utskrivendeLege, pasientId, 3);
+    }
+
+    public int prisAaBetale(){
+        return 0;
+    }
+}
+
+/**
+ * Klasse for P-Resepter.
+ */
+class PResept extends HvitResept{
+
+    static final int RABATT = 108;
+
+    public PResept(Legemiddel legemiddel, Lege utskrivendeLege,
+            int pasientId, int reit){
+        super(legemiddel, utskrivendeLege, pasientId, reit);
+    }
+
+    public int prisAaBetale(){
+        
+        int justertPris = this.legemiddel.hentPris() - RABATT; 
+        if (justertPris > 0){
+            return justertPris;
+        }
+        return 0;
+    }
+}
+
+/**
+ * Klasse for blaa resepter.
+ */
+class BlaaResept extends Resept{
+
+    static final double RABATTFAKTOR = 0.25;
+
+    public BlaaResept(Legemiddel legemiddel, Lege utskrivendeLege,
+            int pasientId, int reit){
+        super(legemiddel, utskrivendeLege, pasientId, reit);
+    }
+
+    public String farge(){
+        return "blaa";
+    }
+
+    public int prisAaBetale(){
+        return ( (int)(this.legemiddel.hentPris() * RABATTFAKTOR));
+    }
+}
+
+/**
+ * Should alternative toString-method be implemented in Superclass?
+ * ... with instanceOf?
+ */
---
+++ obliger/2_oblig2/TestResepter.java
@@ -0,0 +1,38 @@
+public class TestResepter{
+    public static void main(String[] args){
+     
+        /**
+         * OPPRETTER LEGEMIDLER
+         */
+        Vanlig aspirin = new Vanlig("Aspirin", 49, 500);
+        Vanlig ibux = new Vanlig("Ibux", 79, 1000);
+        Vanedannende morfium = new Vanedannende("Morfium", 4000, 50, 10);
+        Narkotisk ketamin = new Narkotisk("Ketamin", 500, 50, 10);
+
+        /**
+         * OPPRETTER LEGE
+         */
+        Lege minLege = new Lege();
+
+        /**
+         * OPPRETTER RESEPTER
+         */
+
+        MilResept resept1 = new MilResept(aspirin, minLege, 666);
+        PResept resept2 = new PResept(ibux, minLege, 123, 5);
+        BlaaResept resept3 = new BlaaResept(morfium, minLege, 007, 1);
+
+        /**
+         * KJOERER NOEN TESTER
+         */
+
+        System.out.println(resept1.toString());
+        System.out.println(resept2.toString());
+        System.out.println(resept3.toString());
+        resept3.bruk(); // resept 3 (morfium boer naa ha Reit: 0)
+        System.out.println(resept2.prisAaBetale()); // burde vaere 0
+        System.out.println(resept3.toString()); // burde vaere ugyldig
+    }
+
+}    
+
