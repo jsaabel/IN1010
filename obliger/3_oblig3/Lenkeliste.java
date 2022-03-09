bf72dfe6a665e276a90b740e54cbad18a884641e
Jonas Saabel <jonassaabel@gmail.com>
2022-03-09 18:56:47 +0100

    start paa fix

--- obliger/3_oblig3/Lenkeliste.java
+++ obliger/3_oblig3/Lenkeliste.java
@@ -1,34 +1,8 @@
 abstract class Lenkeliste<T> implements Liste<T> {
 
-    class Node implements Comparable<Node>{
+    class Node {
         Node neste = null;
         T data;
-
-        @Override
-        public int compareTo(Node annen){
-        
-            if (annen.data instanceof Integer){
-
-                if ((int) data > (int) annen.data){
-                    return 1;}
-                else if ((int) data < (int) annen.data){
-                    return -1;}
-                return 0;
-            }
-
-            else if (annen.data instanceof String){
-
-
-                String foerste = (String) data;
-                String andre = (String) annen.data;
-                
-                return foerste.compareToIgnoreCase(andre);
-                
-                
-            }
-
-            return 0;
-        }
 
         Node(T x){
             data = x;
