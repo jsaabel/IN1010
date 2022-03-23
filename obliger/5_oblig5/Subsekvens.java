13aba586909c7e022b0eeef2b51a11e2a4cb1ad6
jsaabel <jonassaabel@gmail.com@git config --global user.email jonassaabel@gmail.comgit config --global user.email jonassaabel@gmail.com@>
2022-03-23 14:07:04 +0100

    kompilerer uten feil

---
+++ obliger/5_oblig5/Subsekvens.java
@@ -0,0 +1,33 @@
+/**
+ * Klasse som kan ta vare paa en subsekvens og et antall.
+ * ...
+ */
+public class Subsekvens{
+
+    private int forekomster;
+    public final String subsekvens;
+
+    public Subsekvens(String ss){
+        subsekvens = ss; // forekomster 0 eller 1?
+    }
+
+    public int hentForekomster(){
+        return forekomster;
+    }
+
+    // (Temp - se naermere paa implementasjonen paa et senere tidspunkt)
+    public void endreForekomster(int tall){
+        forekomster += tall;
+    }
+
+    @Override
+    public String toString(){
+        return subsekvens + "," + forekomster;
+    }
+
+
+
+
+
+}    
+
---
+++ obliger/5_oblig5/SubsekvensRegister.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SubsekvensRegister{
+
+        private ArrayList<HashMap<String, Subsekvens>> hashBeholder;
+
+        // Konstruktoer
+        public SubsekvensRegister(){
+            hashBeholder = new ArrayList<HashMap<String, Subsekvens>>();
+        }
+
+        // Sett inn HashMap
+        public void settInn(HashMap<String, Subsekvens> hm){
+            hashBeholder.add(hm);
+        }
+        
+        // Ta ut HashMap --- OBS: Skal elementet fjernes eller ei???
+        public HashMap<String, Subsekvens> taUt(){
+            return hashBeholder.remove(0); // foerste element
+        }
+        // Returnere antall HashMaps
+
+}    
+
