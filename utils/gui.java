c6b10e860c5050b61e6051669126d8dc41119a61
jsaabel <jonassaabel@gmail.com@git config --global user.email jonassaabel@gmail.comgit config --global user.email jonassaabel@gmail.com@>
2022-04-26 17:33:07 +0200

    added utils folder + gui snippet

---
+++ utils/gui.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class X {
+    public static void main (String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) { System.exit(1); }
+        JFrame vindu = new JFrame("Xxx");
+        vindu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        vindu.add(panel);
+
+        vindu.pack();
+        vindu.setVisible(true);
+    }
+}
