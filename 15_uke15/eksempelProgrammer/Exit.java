83cd162138b2823d8ef4240f1a8c526a333af294
jsaabel <jonassaabel@gmail.com@git config --global user.email jonassaabel@gmail.comgit config --global user.email jonassaabel@gmail.com@>
2022-04-26 18:00:35 +0200

    gjorde Exit.java mer oversiktlig

--- 15_uke15/eksempelProgrammer/Exit.java
+++ 15_uke15/eksempelProgrammer/Exit.java
@@ -8,22 +8,25 @@ class Exit {
             UIManager.setLookAndFeel(
                 UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (Exception e) { System.exit(1); }
+
         JFrame vindu = new JFrame("Exit");
         vindu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         vindu.add(panel);
 
         JButton exitKnapp = new JButton("Exit");
-	class Stopper implements ActionListener {
-	    @Override
-	    public void actionPerformed (ActionEvent e) {
-		System.exit(0);
-	    }
-	}
-	exitKnapp.addActionListener(new Stopper());
-        panel.add(exitKnapp);
 
-        vindu.pack();
-        vindu.setVisible(true);
+        class Stopper implements ActionListener {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                System.exit(0);
+            }
+        }
+
+	exitKnapp.addActionListener(new Stopper());
+    panel.add(exitKnapp);
+
+    vindu.pack();
+    vindu.setVisible(true);
     }
 }
